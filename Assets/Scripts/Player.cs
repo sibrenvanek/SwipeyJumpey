28824f3d5a462f41367f3565c9adedfe3b4d3565
Niels <nielsvdam@hotmail.com>
2019-09-03 16:11:42 +0200

    Some updates regarding checkpoints and some unassigned variables

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour 
 {
+	private PlayerMovement playerMovement = null;
+
+	private void Awake() {
+		playerMovement = GetComponent<PlayerMovement>();
+	}
 
 	private void OnCollisionEnter2D(Collision2D other) {
 		if(other.gameObject.CompareTag("DeadZone"))
 		{
 			GameManager.Instance.ResetPlayerToCheckpoint();
+		}if(other.gameObject.CompareTag("Checkpoint"))
+		{
+			other.gameObject.GetComponent<Checkpoint>().Check();
+			playerMovement.EnableJump();
 		}
 	}
 }
