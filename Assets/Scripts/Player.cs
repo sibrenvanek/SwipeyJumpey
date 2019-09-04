a0cfcfcf5288fe7cb7543d00b066b170dd26bc0f
Bart in t Veld <bartintveld.contact@gmail.com>
2019-09-04 09:21:41 +0200

    All files formatted

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,22 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour 
+public class Player : MonoBehaviour
 {
-	private PlayerMovement playerMovement = null;
+    private PlayerMovement playerMovement = null;
 
-	private void Awake() {
-		playerMovement = GetComponent<PlayerMovement>();
-	}
+    private void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
 
-	private void OnCollisionEnter2D(Collision2D other) {
-		if(other.gameObject.CompareTag("DeadZone"))
-		{
-			GameManager.Instance.ResetPlayerToCheckpoint();
-		}if(other.gameObject.CompareTag("Checkpoint"))
-		{
-			other.gameObject.GetComponent<Checkpoint>().Check();
-			playerMovement.EnableJump();
-		}
-	}
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("DeadZone"))
+        {
+            GameManager.Instance.ResetPlayerToCheckpoint();
+        }
+        if (other.gameObject.CompareTag("Checkpoint"))
+        {
+            other.gameObject.GetComponent<Checkpoint>().Check();
+            playerMovement.EnableJump();
+        }
+    }
 }
