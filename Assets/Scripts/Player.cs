7eab883e512eec18b7e2001ee648184c82190a5e
Niels <nielsvdam@hotmail.com>
2019-09-04 10:05:56 +0200

    tilemap works, including deadzones and safeground

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,28 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour 
+public class Player : MonoBehaviour
 {
-	private PlayerMovement playerMovement = null;
+    private PlayerMovement playerMovement = null;
 
-	private void Awake() {
-		playerMovement = GetComponent<PlayerMovement>();
-	}
+    private void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
 
-	private void OnCollisionEnter2D(Collision2D other) {
-		if(other.gameObject.CompareTag("DeadZone"))
-		{
-			GameManager.Instance.ResetPlayerToCheckpoint();
-		}else if (other.gameObject.CompareTag("SafeGround"))
-		{
-			playerMovement.EnableJump();
-		}
-	}
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("DeadZone"))
+        {
+            GameManager.Instance.ResetPlayerToCheckpoint();
+        }
+        else if (other.gameObject.CompareTag("SafeGround"))
+        {
+            playerMovement.EnableJump();
+        }
+    }
 
-	private void OnTriggerEnter2D(Collider2D other) {
-		if(other.gameObject.CompareTag("Checkpoint"))
-		{
-			other.gameObject.GetComponent<Checkpoint>().Check();
-		}
-	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Checkpoint"))
+        {
+            other.gameObject.GetComponent<Checkpoint>().Check();
+        }
+    }
 }
