392c5b4e6b77ce398889fb29f2aefbd692ca725f
Niels <nielsvdam@hotmail.com>
2019-09-09 14:23:21 +0200

    fuelUI

---
+++ Assets/Scripts/FuelUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelUI : MonoBehaviour
+{
+    [SerializeField] private Image fuelImage = null;
+    private float secondsToEmpty = 2f;
+    private PlayerMovement playerMovement = null;
+
+
+    private void Update() 
+    {
+        
+    }
+
+    private void OnSlowMotionActivated()
+    {
+
+    }
+
+    private void OnSlowMotionDeActivated()
+    {
+
+    }
+}
