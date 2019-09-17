ea70474a65ccb607137b1d0678b6de3ef1ac847d
tjbuiten <timobuitenhuis@gmail.com>
2019-09-17 14:58:07 +0200

    added a mouseposition indicator

---
+++ Assets/Scripts/UI/MouseIndicator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseIndicator : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer = null;
+    private Color defaultColor = Color.white;
+    [SerializeField] private Color cancelColor = Color.black;
+    private bool cancelActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        defaultColor = spriteRenderer.color;
+    }
+
+    private void OnMouseEnter()
+    {
+        if (cancelActive)
+            return;
+
+        cancelActive = true;
+        spriteRenderer.color = cancelColor;
+    }
+
+    private void OnMouseExit()
+    {
+        if (!cancelActive)
+            return;
+
+        cancelActive = false;
+        spriteRenderer.color = defaultColor;
+    }
+}
