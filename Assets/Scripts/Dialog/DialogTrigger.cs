968cb033dc559d386d63f7e71f8901f1d2c25487
Bart in t Veld <bart.int.007@gmail.com>
2019-09-17 10:04:18 +0300

    Added a trigger that disables input

---
+++ Assets/Scripts/Dialog/DialogTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (OtherIsPlayer(other))
+        {
+            KillPlayerMovement(other);
+        }
+    }
+
+    private bool OtherIsPlayer(Collider2D other)
+    {
+        GameObject parent = other.transform.parent.gameObject;
+        return parent != null && parent.CompareTag("Player");
+    }
+
+    private void KillPlayerMovement(Collider2D other)
+    {
+        GameObject player = other.transform.parent.gameObject;
+        PlayerMovement playerMovement = player.GetComponent<PlayerMovement>();
+
+        playerMovement.Disable();
+        playerMovement.KillVelocity();
+    }
+}
