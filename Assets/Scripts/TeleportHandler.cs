be3a7771be32f8b2f5ed1f318509d67e358278d8
tjbuiten <timobuitenhuis@gmail.com>
2019-10-02 10:20:15 +0200

    created teleporters

---
+++ Assets/Scripts/TeleportHandler.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportHandler : MonoBehaviour
+{
+    private PlayerMovement player = null;
+    [SerializeField] private Teleporter teleporterOne = null;
+    [SerializeField] private Teleporter teleporterTwo = null;
+    [SerializeField] private bool reverseY = false;
+    [SerializeField] private bool reverseX = false;
+    private bool onOne = false;
+    private bool onTwo = false;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+        teleporterOne.OnTeleporting += delegate { teleportOne(); };
+        teleporterTwo.OnTeleporting += delegate { teleportTwo(); };
+        teleporterOne.OnTriggerExit += delegate { leftTeleporterOne(); };
+        teleporterTwo.OnTriggerExit += delegate { leftTeleporterTwo(); };
+    }
+
+    private void teleportOne()
+    {
+        if (onOne || onTwo)
+            return;
+
+        onTwo = true;
+
+        Rigidbody2D playerRigidbody = player.GetComponentInParent<Rigidbody2D>();
+
+        if (reverseY)
+            playerRigidbody.velocity += new Vector2(0, playerRigidbody.velocity.y * -2);
+
+        if (reverseX)
+            playerRigidbody.velocity += new Vector2(0, playerRigidbody.velocity.x * -2);
+
+        player.gameObject.transform.position = teleporterTwo.transform.position;
+    }
+
+    private void teleportTwo()
+    {
+        if (onOne || onTwo)
+            return;
+
+        onOne = true;
+
+        Rigidbody2D playerRigidbody = player.GetComponentInParent<Rigidbody2D>();
+
+        if (reverseY)
+            playerRigidbody.velocity += new Vector2(0, playerRigidbody.velocity.y * -2);
+
+        if (reverseX)
+            playerRigidbody.velocity += new Vector2(0, playerRigidbody.velocity.x * -2);
+        player.gameObject.transform.position = teleporterOne.transform.position;
+    }
+
+    private void leftTeleporterOne()
+    {
+        onOne = false;
+    }
+
+    private void leftTeleporterTwo()
+    {
+        onTwo = false;
+    }
+}
