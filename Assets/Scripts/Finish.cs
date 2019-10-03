a8ba7564ce10b94db636aa1baf69d4e2b43688da
Sibren <0924655@hr.nl>
2019-10-03 11:40:44 +0200

    fix finish

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Finish : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collider)
     {
         WorldManager worldManager = FindObjectOfType<WorldManager>();
         GetComponentInChildren<ParticleSystem>().Play();
