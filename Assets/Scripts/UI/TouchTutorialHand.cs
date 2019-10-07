d668a707e609c6fe9405b79a8ffdbd39ac119e83
Niels <nielsvdam@hotmail.com>
2019-10-07 16:02:07 +0200

    remove touch tutorial hand on click

---
+++ Assets/Scripts/UI/TouchTutorialHand.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TouchTutorialHand : MonoBehaviour
+{
+    private bool active = true;
+
+    private void Update() 
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(active)
+            {
+                StartCoroutine(FadeAndDeactivate());
+            }
+        }    
+    }
+
+    private IEnumerator FadeAndDeactivate()
+    {
+        GetComponent<SpriteRenderer>().DOFade(0, 2f);
+        active = false;
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+    }
+}
