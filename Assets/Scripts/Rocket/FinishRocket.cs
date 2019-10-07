1f0c4e857f5bb4d76044b24b35d3622769c79e48
Niels <nielsvdam@hotmail.com>
2019-10-07 09:13:47 +0200

    rocket is flying!

---
+++ Assets/Scripts/Rocket/FinishRocket.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FinishRocket : MonoBehaviour
+{
+    
+
+    private bool flying = false;
+    [SerializeField] private float speed = 2f;
+
+    private void FixedUpdate() 
+    {
+        if(flying)
+            transform.Translate(Vector3.up * speed * Time.deltaTime, Space.World);    
+    }
+
+    public void Fly(Action callback)
+    {
+        StartCoroutine(FlyAndCallback(callback));
+    }
+
+    private IEnumerator FlyAndCallback(Action callback)
+    {
+        flying = true;
+        yield return new WaitForSeconds(2f);
+        flying = false;
+        callback();
+    }
+}
