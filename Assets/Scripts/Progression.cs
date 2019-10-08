586bc9c441af524eb548079df696c78a7328cec4
tjbuiten <timobuitenhuis@gmail.com>
2019-10-08 13:35:39 +0200

    removed unused imports

--- Assets/Scripts/Progression.cs
+++ Assets/Scripts/Progression.cs
@@ -1,10 +1,8 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.Android;
-using UnityEngine.Networking;
 
 public class Progression
 {
