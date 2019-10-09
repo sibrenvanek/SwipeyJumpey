206278ca1da4f68d158902318a44f096c77a91a2
Niels <nielsvdam@hotmail.com>
2019-10-09 12:03:28 +0200

    klaar

--- Assets/Scripts/Booster/Booster.cs
+++ Assets/Scripts/Booster/Booster.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-[RequireComponent(typeof(Collider2D), typeof(SpriteRenderer))]
+[RequireComponent(typeof(Collider2D), typeof(SpriteRenderer), typeof(AudioSource))]
 public abstract class Booster : MonoBehaviour
 {
     protected SpriteRenderer spriteRenderer = null;
     protected new Collider2D collider = null;
+    protected AudioSource audioSource = null;
 
     [Header("Base options")]
     [SerializeField] private Animator animator = null;
     [SerializeField] private float force = 2f;
+    [SerializeField] private AudioClip boostSFX = null;
 
     protected virtual void Awake() 
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         collider = GetComponent<Collider2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     protected abstract void Activate(GameObject player);
@@ -28,7 +31,7 @@ public abstract class Booster : MonoBehaviour
 
         rigidbody.transform.position = transform.position;
         rigidbody.velocity = root.up * force;
-        //BoostAnimation();
+        audioSource.PlayOneShot(boostSFX);
     }
 
     protected virtual void Disable()
@@ -41,10 +44,6 @@ public abstract class Booster : MonoBehaviour
         collider.enabled = true;
     }
 
-    private void BoostAnimation()
-    {
-        animator.SetTrigger("Boost");
-    }
     
     private void OnTriggerEnter2D(Collider2D other)
     {
