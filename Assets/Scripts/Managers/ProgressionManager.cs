bd8185ad1f9d87c2d6b0925277cdc010eacb94b1
Bart in t Veld <bartintveld.contact@gmail.com>
2019-10-09 10:21:32 +0200

    Changed player settings

--- Assets/Scripts/Managers/ProgressionManager.cs
+++ Assets/Scripts/Managers/ProgressionManager.cs
@@ -10,7 +10,7 @@ public class ProgressionManager : MonoBehaviour
     public static ProgressionManager Instance;
 
     private Progression progression;
-    private readonly int ID = 14;
+    private readonly int ID = 15;
 
     private void Awake()
     {
